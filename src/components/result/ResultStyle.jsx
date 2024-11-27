58995f0588f2ebe859763ee218d7acf585c0a60a
K's_Fe <119591236+5647kr@users.noreply.github.com>
2024-11-27 12:20:30 +0900

    [🖌 Design] 10000HoursRule Result 컴포넌트 UI 작업

---
+++ src/components/result/ResultStyle.jsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+const ResultWrap = styled.div`
+  margin-top: 60px;
+
+  p {
+    font-size: 24px;
+    font-family: "Black Han Sans";
+  }
+
+  span {
+    font-size: 60px;
+    margin: 0 8px;
+    vertical-align: middle;
+  }
+`
+
+export {ResultWrap}
+
